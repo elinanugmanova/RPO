d7f7a7c932f43b1afdc43bc188b4a1e4d98b90ef
Elina <username@yandex.ru>
2025-03-13 16:58:33 +0300

    End Laboratory work 2

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,13 +1,24 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -25,14 +36,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         int res = initRng(); //не убирать вообще!!!!
-        byte[] key = randomBytes(16);
-        byte[] string = randomBytes(10);
-
-        byte[] encode = encrypt(key, string);
-        byte[] decode = decrypt(key, string);
+//        byte[] key = randomBytes(16);
+//        byte[] string = randomBytes(10);
+//
+//        byte[] encode = encrypt(key, string);
+//        byte[] decode = decrypt(key, string);
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        String pin = data.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        //TextView tv = binding.sampleText;
+        //tv.setText(stringFromJNI());
     }
 
     /**
@@ -40,8 +65,28 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
+
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        } catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+    }
+
+    public void onButtonClick(View v) {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
 }
